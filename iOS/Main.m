6d843088cc5b4a8e438bdc2b29174e5043cfd1bd
d39611919-oss <d39611919@gmail.com>
2026-05-14 15:03:39 +0300

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
+#define API_URL @"https://51.85.96.25:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
